15b2b598a6c5aa1c65c8c8270ddfe55dae77ae87
Kukatla Vishalakshmi <Kukatla.Vishalakshmi@ibm.com>
2022-03-07 10:09:37 +0530

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System;
+namespace AccessSpecifiers
+{
+    class PublicTest
+    {
+        public string name = "vishu kukatla";
+        public void Msg(string msg)
+        {
+            Console.WriteLine("hi " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicTest publicTest = new PublicTest();
+            // Accessing public variable  
+            Console.WriteLine("hi " + publicTest.name);
+            // Accessing public function  
+            publicTest.Msg("chinni");
+            Console.ReadLine();
+        }
+    }
+}
